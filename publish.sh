1197f4c8bdd317a708627ffc8431a6984ae3a351
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-04-22 23:31:48 -0400

    adapt publish script for gallery

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -r ../svgcraft.github.io/* && cp -r img *.js *.json *.html *.css AUTHORS LICENSE ../svgcraft.github.io/
+rm -r ../svgcraft.github.io/* && cp -r img *.js *.json *.html *.css AUTHORS LICENSE ../svgcraft.github.io/ && mkdir ../svgcraft.github.io/gallery && cp ./gallery/*.svg ../svgcraft.github.io/gallery/
