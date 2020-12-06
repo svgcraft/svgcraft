69d3086f7aaa03628fecc10e5ced4f156290f540
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-12-06 01:14:49 -0500

    refactor layout to make video positionable

--- handtrack/track.js
+++ handtrack/track.js
@@ -39,13 +39,60 @@ function toggleVideo() {
     }
 }
 
-let showArms = false;
-let showHandboxes = false;
+// svg units
+const arenaWidth = 16;
+const arenaHeight = 9;
+let playerWidth = 2;
+let playerHeight = 1.5;
+let playerX = arenaWidth / 2;
+let playerY = arenaHeight / 2;
+
+// CSS pixels
+const arenaBorder = 20; 
+
+// handtrack.js video processing pixels
+const videoWidth = 640;
+const videoHeight = 480;
+
+let pxPerUnit = 123;
+
+function setDim(elem, w, h) {
+    elem.style.width = pxPerUnit * w + "px";
+    elem.style.height = pxPerUnit * h + "px";
+}
+
+function setPos(elem, x, y) {
+    elem.style.left = pxPerUnit * x + "px";
+    elem.style.top = pxPerUnit * y + "px";
+}
+
+function positionArenaAndPlayer() {
+    const availableWidth = window.innerWidth - 2 * arenaBorder;
+    const availableHeight = window.innerHeight - 2 * arenaBorder;
+    pxPerUnit = Math.min(availableWidth / arenaWidth, availableHeight / arenaHeight);
+
+    setDim(I("arenaWrapper"), arenaWidth, arenaHeight);
+    setDim(I("arenaBG"),      arenaWidth, arenaHeight);
+    setDim(I("arenaFG"),      arenaWidth, arenaHeight);
+
+    const x1 = playerX - playerWidth / 2;
+    const y1 = playerY - playerHeight / 2;
+    setPos(I("myvideo"), x1, y1);
+    setDim(I("myvideo"), playerWidth, playerHeight);
+
+    I("overlayG").setAttribute("transform", `translate(${x1}, ${y1})`);
+}
+
+let showHead = true;
+let showArms = true;
+let showHandboxes = true;
 let showHands = true;
+
 let handScaleFactor = 1.6;
 
-function hideArms() {
+function hideStickfigure() {
     for (var i = 0; i < 2; i++) {
+        I("head").style.display = "none";
         I("arm" + i).style.display = "none";
         I("handbox" + i).style.display = "none";
         I("hand" + i).style.display = "none";
@@ -53,28 +100,34 @@ function hideArms() {
     }
 }
 
-function placeArms(predictions) {
-    hideArms();
+function showStickfigure(predictions) {
+    hideStickfigure();
+    if (showHead) {
+        I("head").style.display = "";
+    }
     for (var i = 0; i < predictions.length; i++) {
-        const minx = predictions[i].bbox[0];
-        const miny = predictions[i].bbox[1];
-        const w = predictions[i].bbox[2];
-        const h = predictions[i].bbox[3];
+        // in svg units
+        const minx = predictions[i].bbox[0] / videoWidth * playerWidth;
+        const miny = predictions[i].bbox[1] / videoHeight * playerHeight;
+        const w = predictions[i].bbox[2] / videoWidth * playerWidth;
+        const h = predictions[i].bbox[3] / videoHeight * playerHeight;
         const cx = minx + w / 2;
         const cy = miny + h / 2;
+
+        const handClip = I("handClip" + i);
+        handClip.style.display = "";
+        handClip.setAttribute("cx", 1.0 - cx / playerWidth); // mirror
+        handClip.setAttribute("cy", cy / playerHeight);
+        handClip.setAttribute("rx", w / 2 / playerWidth * handScaleFactor);
+        handClip.setAttribute("ry", h / 2 / playerHeight * handScaleFactor);
+
         if (showHands) {
             const hand = I("hand" + i);
             hand.style.display = "";
             hand.setAttribute("cx", cx);
             hand.setAttribute("cy", cy);
-            hand.setAttribute("rx", w/2);
-            hand.setAttribute("ry", h/2);
-            const handClip = I("handClip" + i);
-            handClip.style.display = "";
-            handClip.setAttribute("cx", 1.0 - cx/640);
-            handClip.setAttribute("cy", cy/480);
-            handClip.setAttribute("rx", w/2/640*handScaleFactor);
-            handClip.setAttribute("ry", h/2/480*handScaleFactor);
+            hand.setAttribute("rx", w / 2 * handScaleFactor);
+            hand.setAttribute("ry", h / 2 * handScaleFactor);
         }
         if (showArms) {
             const arm = I("arm" + i);
@@ -98,7 +151,8 @@ function runDetection() {
         console.log("predictions: ", predictions);
         //const context = I("canvas").getContext("2d");
         //model.renderPredictions(predictions, I("canvas"), context, I("myvideo"));
-        placeArms(predictions);
+        positionArenaAndPlayer();
+        showStickfigure(predictions);
         if (isVideo) {
             requestAnimationFrame(runDetection);
         }
